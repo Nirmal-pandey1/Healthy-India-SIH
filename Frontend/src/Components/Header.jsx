beb7cac280ee216a4ad9bb97c8294e2923726a07
Nirmal Pandey <nirmalpandey680@gmail.com>
2025-08-24 14:29:35 +0530

    some  more changes

---
+++ Frontend/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../App.css'
+import logo from '../assets/logo2.png'
+function Header() {
+  return (
+        <div className='shadow'>
+        <nav className='center py-3'>
+            <div>
+                <img src={logo} alt="" className="w-auto  h-13" />
+            </div>
+        </nav>
+        </div>
+    
+  )
+}
+
+export default Header
