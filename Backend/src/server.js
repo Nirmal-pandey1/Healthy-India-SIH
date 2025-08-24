693341d457a0369c226eee249616ad65d05d7922
Subodh Pathak <sptnp04@gmail.com>
2025-08-24 13:48:11 +0530

    db and server.js added

---
+++ Backend/src/server.js
@@ -0,0 +1,12 @@
+
+require('dotenv').config({ path: '../.env' });
+const express = require('express');
+const connectDB = require('./config/db'); 
+const app = express();
+connectDB();
+app.get('/', (req, res) => {
+res.send('API is running...');
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
